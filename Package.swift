3364fa40d40735743619ed6a27020ad7f0f60660
Alimulla Shaik <ali@ujet.co>
2025-11-26 09:34:47 -0700

    Add 3.2.0 RC packages

--- Package.swift
+++ Package.swift
@@ -22,26 +22,26 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "CCAIKit",
-      url: "https://sdk.ujet.co/ccaip/ios-staging/3.1.0-rc.02/CCAIKit.xcframework.zip",
-      checksum: "4dbe1fdc4a7ae2efa9b91d07d2e5632c85494af98cd9ba7e4bf877e99a43c594"
+      url: "https://sdk.ujet.co/ccaip/ios-staging/3.2.0-rc.2/CCAIKit.xcframework.zip",
+      checksum: "93d9915080b5fb0fbdb6bbbc8b128952bec4c29942892dd07c886435c872e079"
     ),
 
     .binaryTarget(
       name: "CCAIChat",
-      url: "https://sdk.ujet.co/ccaip/ios-staging/3.1.0-rc.02/CCAIChat.xcframework.zip",
-      checksum: "25f02d8ecade8519f8dadd2313863798415e2427847fe6d6cdb3c4b21798edcf"
+      url: "https://sdk.ujet.co/ccaip/ios-staging/3.2.0-rc.2/CCAIChat.xcframework.zip",
+      checksum: "fbbe4dcb8fedf1b648058b9aa5269cfa49aa582cde0054e699442fc202de3376"
     ),
 
     .binaryTarget(
       name: "CCAIChatRed",
-      url: "https://sdk.ujet.co/ccaip/ios-staging/3.1.0-rc.02/CCAIChatRed.xcframework.zip",
-      checksum: "276318426b63c276014b57b064ff5962ec10f5c088aa8b97ec9a354c7db77a91"
+      url: "https://sdk.ujet.co/ccaip/ios-staging/3.2.0-rc.2/CCAIChatRed.xcframework.zip",
+      checksum: "8db2396d8c79084f04e6c949af358305246daa47fdd8cf937465af7841068768"
     ),
 
     .binaryTarget(
       name: "CCAIScreenShare",
-      url: "https://sdk.ujet.co/ccaip/ios-staging/3.1.0-rc.02/CCAIScreenShare.xcframework.zip",
-      checksum: "db2990601f7eccc65bfab943c37aef24643741cbb26e82bbf2458d5dc171d88f"
+      url: "https://sdk.ujet.co/ccaip/ios-staging/3.2.0-rc.2/CCAIScreenShare.xcframework.zip",
+      checksum: "4d01f94578aa27e172593fa26237cf9b970cafa76de33015b2203e045e5b6e17"
     ),
 
     .target(
